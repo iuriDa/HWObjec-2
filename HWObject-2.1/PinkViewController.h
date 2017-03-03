ff22d89e5e80afbbf51e7e31d7795eded423f8fb
iurii Danylchenko <iuriida@outlook.com>
2017-03-03 12:45:26 +0100

    Task#2 return Task#1 and add navigation controller with two views (Yellow and Pink)

---
+++ HWObject-2.1/PinkViewController.h
@@ -0,0 +1,13 @@
+//
+//  PinkViewController.h
+//  HWObject-2.1
+//
+//  Created by Inna Lymar on 03.03.17.
+//  Copyright © 2017 iurii Danylchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PinkViewController : UIViewController
+
+@end
---
+++ HWObject-2.1/PinkViewController.m
@@ -0,0 +1,37 @@
+//
+//  PinkViewController.m
+//  HWObject-2.1
+//
+//  Created by Inna Lymar on 03.03.17.
+//  Copyright © 2017 iurii Danylchenko. All rights reserved.
+//
+
+#import "PinkViewController.h"
+
+@interface PinkViewController ()
+
+@end
+
+@implementation PinkViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
